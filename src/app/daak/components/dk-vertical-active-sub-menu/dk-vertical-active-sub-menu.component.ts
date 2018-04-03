ba46dfcaf475754dd73d26f164342d2a3bf5907c
davood <daak1365@yahoo.com>
2018-04-03 13:43:47 +0430

    resolve active menu click

--- src/app/daak/components/dk-vertical-active-sub-menu/dk-vertical-active-sub-menu.component.ts
+++ src/app/daak/components/dk-vertical-active-sub-menu/dk-vertical-active-sub-menu.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {DkVerticalMenuComponent} from '../dk-vertical-menu/dk-vertical-menu.component';
+import {DaakService} from "../../services/daak.service";
 
 @Component({
   selector: 'dk-v-active-sub-menu',
@@ -9,9 +11,16 @@ export class DkVerticalActiveSubMenuComponent implements OnInit {
   @Input('url') url: string;
   @Input('icon') icon: string;
   @Input('title') title: string;
-  constructor() { }
+  constructor(@Inject(DkVerticalMenuComponent) private parent: DkVerticalMenuComponent,
+               private daakService: DaakService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  activeClick(event) {
+    event.stopPropagation();
+    if (!this.daakService.yScrolled) {
+      this.parent.hide();
+    }
+  }
 }
